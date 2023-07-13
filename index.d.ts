f7d0ab77df7a9c7fd9c682f2e5bb16882a107812
Beth Hitch <bethany.e.hitch@gmail.com>
2023-07-13 15:42:19 -0700

    Handle symbol keys and keys of arrays

--- index.d.ts
+++ index.d.ts
@@ -1,20 +1,30 @@
 interface ObjectConstructor {
-    keys<T>(o: T): (keyof T)[];
+  keys<T extends readonly any[]>(o: T): {
+    [K in keyof Required<T>]: `${K}`;
+  }[keyof T & number][];
+  keys<T extends {}>(o: T): `${Exclude<keyof T, symbol>}`[];
 
-    entries<T>(o: T): {
-        [K in keyof Required<T>]: [K, T[K]];
-    }[keyof T][];
+  entries<T extends readonly any[]>(o: T): {
+    [K in keyof Required<T>]: [`${K}`, T[K]];
+  }[keyof T & number][];
+  entries<T extends {}>(o: T): {
+    [K in Exclude<keyof Required<T>, symbol>]: [`${K}`, T[K]];
+  }[Exclude<keyof T, symbol>][];
 
-    fromEntries<T extends readonly (readonly [PropertyKey, unknown])[]>(entries: T): TypesScriptMagic.MakeUndefinedValuesOptional<{
-        [K in T[number][0]]: Extract<T[number], readonly [K, any]>[1];
-    }>;
+  fromEntries<T extends readonly (readonly [PropertyKey, unknown])[]>(entries: T): TSM.MakeUndefinedValuesOptional<{
+    [K in T[number][0]]: Extract<T[number], readonly [K, any]>[1];
+  }>;
 }
 
-/** A namespace for more complicated types, so as not to pollute the global namespace. */
-declare namespace TypesScriptMagic {
-    type UndefinedKeys<T> = {
-        [K in keyof T]-?: undefined extends T[K] ? T[K] extends undefined ? never : K : never;
-    }[keyof T];
+/**
+ * TypeScriptMagic
+ *
+ * A namespace for more complicated utility types, so as not to pollute the global namespace.
+ */
+declare namespace TSM {
+  type UndefinedKeys<T> = {
+    [K in keyof T]-?: undefined extends T[K] ? T[K] extends undefined ? never : K : never;
+  }[keyof T];
 
-    type MakeUndefinedValuesOptional<T> = Omit<T, UndefinedKeys<T>> & Partial<Pick<T, UndefinedKeys<T>>>;
+  type MakeUndefinedValuesOptional<T> = Omit<T, UndefinedKeys<T>> & Partial<Pick<T, UndefinedKeys<T>>>;
 }
--- test.ts
+++ test.ts
@@ -1,14 +1,17 @@
+const TestSymbol = Symbol('test');
+
 // Object.keys() tests
-(): ('a' | 'b' | 'c')[] => Object.keys({ a: 1, b: true, c: { x: 1 } });
-(): ('a' | 'b' | 'c')[] => Object.keys({ a: 1, b: true, c: { x: 1 } } as const);
+(): ('a' | 'b' | 'c' | '0')[] => Object.keys({ a: 1, b: true, c: { x: 1 }, 0: false });
+(): ('a' | 'b' | 'c' | typeof TestSymbol)[] => Object.keys({ a: 1, b: true, c: { x: 1 }, [TestSymbol]: [] } as const);
 (): ('a' | 'b')[] => Object.keys<{ a: 1, b?: true }>({ a: 1 });
+(): ('0' | '1' | '2')[] => Object.keys([1, 2, 3] as const);
 
 // Object.entries() tests
-(): (['a', 1] | ['b', true] | ['c', { x: 1 }])[] => Object.entries({ a: 1, b: true, c: { x: 1 } } as const);
-(): (['a', number] | ['b', boolean] | ['c', { x: number }])[] => Object.entries({ a: 1, b: true, c: { x: 1 } });
+(): (['a', 1] | ['b', true] | ['c', { x: 1 }] | [typeof TestSymbol, 2])[] => Object.entries({ a: 1, b: true, c: { x: 1 }, [TestSymbol]: 2 } as const);
+(): (['a', number] | ['b', boolean] | ['c', { x: number }] | [typeof TestSymbol, number])[] => Object.entries({ a: 1, b: true, c: { x: 1 }, [TestSymbol]: 2 });
 (): (['a', number] | ['b', boolean | undefined])[] => Object.entries<{ a: 1, b?: true }>({ a: 1 });
 
 // Object.fromEntries() tests
-(): { a: 1; b: true; c: { x: 1; }; } => Object.fromEntries([['a', 1], ['b', true], ['c', { x: 1 }]] as const);
+(): { a: 1; b: true; c: { x: 1; }; [TestSymbol]: 2 } => Object.fromEntries([['a', 1], ['b', true], ['c', { x: 1 }], [TestSymbol, 2]] as const);
 (): { a: number; b: boolean; c: { x: number; }; } => Object.fromEntries(Object.entries({ a: 1, b: true, c: { x: 1 } }));
 (): { a: number; b?: boolean; } => Object.fromEntries(Object.entries<{ a: 1, b?: true }>({ a: 1 }));
