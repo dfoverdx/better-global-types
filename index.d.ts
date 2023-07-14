4d0a03eb98b3682eb13193be28c4fb68d5708bd2
Beth Hitch <bethany.e.hitch@gmail.com>
2023-07-13 17:13:06 -0700

    Fixed cases for non-tuple arrays Made tests more robust

--- index.d.ts
+++ index.d.ts
@@ -1,15 +1,9 @@
 interface ObjectConstructor {
-  keys<T extends readonly any[]>(o: T): {
-    [K in keyof Required<T>]: `${K}`;
-  }[keyof T & number][];
+  keys<T extends readonly any[]>(o: T): `${TSM.IterableArrayKeys<T>}`[];
   keys<T extends {}>(o: T): `${Exclude<keyof T, symbol>}`[];
 
-  entries<T extends readonly any[]>(o: T): {
-    [K in keyof Required<T>]: [`${K}`, T[K]];
-  }[keyof T & number][];
-  entries<T extends {}>(o: T): {
-    [K in Exclude<keyof Required<T>, symbol>]: [`${K}`, T[K]];
-  }[Exclude<keyof T, symbol>][];
+  entries<T extends readonly any[]>(o: T): { [K in keyof Required<T> & (string | number)]: [`${K}`, T[K]]; }[TSM.IterableArrayKeys<T>][];
+  entries<T extends {}>(o: T): { [K in Exclude<keyof Required<T>, symbol>]: [`${K}`, T[K]]; }[Exclude<keyof T, symbol>][];
 
   fromEntries<T extends readonly (readonly [PropertyKey, unknown])[]>(entries: T): TSM.MakeUndefinedValuesOptional<{
     [K in T[number][0]]: Extract<T[number], readonly [K, any]>[1];
@@ -27,4 +21,12 @@ declare namespace TSM {
   }[keyof T];
 
   type MakeUndefinedValuesOptional<T> = Omit<T, UndefinedKeys<T>> & Partial<Pick<T, UndefinedKeys<T>>>;
+
+  type IterableArrayKeys<T extends readonly any[]> =
+    | Exclude<keyof Required<T>, symbol | keyof any[]>
+    | (
+        T extends readonly []
+          ? never
+          : T extends readonly [infer _, ...any] ? never : number
+      );
 }
--- test.ts
+++ test.ts
@@ -1,17 +1,123 @@
 const TestSymbol = Symbol('test');
 
+type AugmentedArray<T extends readonly any[]> = T & {
+  foo?: string;
+  [TestSymbol]: number;
+}
+
+const augArr = [1, 2, 3] as AugmentedArray<[1, 2, 3]>;
+
 // Object.keys() tests
-(): ('a' | 'b' | 'c' | '0')[] => Object.keys({ a: 1, b: true, c: { x: 1 }, 0: false });
-(): ('a' | 'b' | 'c' | typeof TestSymbol)[] => Object.keys({ a: 1, b: true, c: { x: 1 }, [TestSymbol]: [] } as const);
-(): ('a' | 'b')[] => Object.keys<{ a: 1, b?: true }>({ a: 1 });
-(): ('0' | '1' | '2')[] => Object.keys([1, 2, 3] as const);
+{
+  const r = Object.keys({ a: 1, b: true, c: { x: 1 }, 0: false, [TestSymbol]: [] });
+  const _: typeof r[number][] = [
+    'a', 'b', 'c', '0',
+    // @ts-expect-error
+    TestSymbol,
+    // @ts-expect-error
+    'd',
+  ];
+}
+{
+  const r = Object.keys<{ a: 1, b?: true }>({ a: 1 });
+  const _: typeof r[number][] = [
+    'a', 'b',
+    // @ts-expect-error
+    'c',
+  ];
+}
+{
+  const r = Object.keys([1, 2, 3] as const);
+  const _: typeof r[number][] = [
+    '0', '1', '2',
+    // @ts-expect-error
+    '3',
+    // @ts-expect-error
+    'foo',
+  ];
+}
+{
+  const r = Object.keys<number[]>([1, 2, 3]);
+  const _: typeof r[number][] = [
+    '0', '1', '2', '4',
+    // @ts-expect-error
+    'foo',
+  ];
+}
+{
+  const r = Object.keys(augArr);
+  const _: typeof r[number][] = [
+    '0', '1', '2', 'foo',
+    // @ts-expect-error
+    TestSymbol,
+    // @ts-expect-error
+    'd',
+  ];
+}
 
 // Object.entries() tests
-(): (['a', 1] | ['b', true] | ['c', { x: 1 }] | [typeof TestSymbol, 2])[] => Object.entries({ a: 1, b: true, c: { x: 1 }, [TestSymbol]: 2 } as const);
-(): (['a', number] | ['b', boolean] | ['c', { x: number }] | [typeof TestSymbol, number])[] => Object.entries({ a: 1, b: true, c: { x: 1 }, [TestSymbol]: 2 });
-(): (['a', number] | ['b', boolean | undefined])[] => Object.entries<{ a: 1, b?: true }>({ a: 1 });
+{
+  const r = Object.entries({ a: 1, b: true, c: { x: 1 }, [TestSymbol]: 2 } as const);
+  const _: typeof r[number][] = [
+    ['a', 1], ['b', true], ['c', { x: 1 }],
+    // @ts-expect-error
+    [TestSymbol, 2],
+    // @ts-expect-error
+    ['d', 3],
+    // @ts-expect-error
+    ['a', 'b'],
+  ]
+}
+{
+  const r = Object.entries({ a: 1, b: true, c: { x: 1 }, [TestSymbol]: 2 });
+  const _: typeof r[number][] = [
+    ['a', 0], ['b', false], ['c', { x: 2 }],
+    // @ts-expect-error
+    ['c', { x: 'a'} ],
+    // @ts-expect-error
+    [TestSymbol, 2],
+    // @ts-expect-error
+    ['d', 3],
+  ];
+}
+{
+  const r = Object.entries<{ a: 1, b?: true }>({ a: 1 });
+  const _: typeof r[number][] = [
+    ['a', 1], ['b', undefined], ['b', true],
+    // @ts-expect-error
+    undefined,
+  ];
+}
+{
+  const r = Object.entries([1, 2, 3] as const);
+  const _: typeof r[number][] = [
+    ['0', 1], ['1', 2], ['2', 3],
+    // @ts-expect-error
+    ['3', 4],
+    // @ts-expect-error
+    [0, 1],
+  ];
+}
+{
+  const r = Object.entries<number[]>([1, 2, 3]);
+  const _: typeof r[number][] = [
+    ['0', 1], ['1', 2], ['2', 3], ['3', 4],
+    // @ts-expect-error
+    [1, 1]
+  ];
+}
+{
+  const r = Object.entries(augArr);
+  const _: typeof r[number][] = [
+    ['0', 1], ['1', 2], ['2', 3], ['foo', 'bar'], ['foo', undefined],
+    // @ts-expect-error
+    [TestSymbol, 2],
+  ];
+}
 
 // Object.fromEntries() tests
 (): { a: 1; b: true; c: { x: 1; }; [TestSymbol]: 2 } => Object.fromEntries([['a', 1], ['b', true], ['c', { x: 1 }], [TestSymbol, 2]] as const);
 (): { a: number; b: boolean; c: { x: number; }; } => Object.fromEntries(Object.entries({ a: 1, b: true, c: { x: 1 } }));
 (): { a: number; b?: boolean; } => Object.fromEntries(Object.entries<{ a: 1, b?: true }>({ a: 1 }));
+// @ts-expect-error
+(): { a: number; b: boolean; } => Object.fromEntries(Object.entries<{ a: 1, b?: true }>({ a: 1 }));
