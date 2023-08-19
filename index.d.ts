5d3b2874e883f81d373fb1da89987defedca5821
Beth Hitch <bethany.e.hitch@gmail.com>
2023-08-18 18:46:36 -0700

    Add TSM.DeepReadonly, TSM.IsAny, and TSM.isNever Add overloads for parseInt and parseNumber
    Add comments to TSM types
    Add a test case to Object.keys(Tuple) and Object.entries(Tuple)
    Add tests for TSM types

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,8 @@
 interface ObjectConstructor {
-  keys<T extends readonly any[]>(o: T): `${TSM.IterableArrayKeys<T>}`[];
+  keys<T extends readonly any[]>(o: T): `${TSM.ArrayObjectKeys<T>}`[];
   keys<T extends {}>(o: T): `${Exclude<keyof T, symbol>}`[];
 
-  entries<T extends readonly any[]>(o: T): { [K in keyof Required<T> & (string | number)]: [`${K}`, T[K]]; }[TSM.IterableArrayKeys<T>][];
+  entries<T extends readonly any[]>(o: T): { [K in keyof Required<T> & (string | number)]: [`${K}`, T[K]]; }[TSM.ArrayObjectKeys<T>][];
   entries<T extends {}>(o: T): { [K in Exclude<keyof Required<T>, symbol>]: [`${K}`, T[K]]; }[Exclude<keyof T, symbol>][];
 
   fromEntries<T extends readonly (readonly [PropertyKey, any])[]>(entries: T): TSM.MakeUndefinedValuesOptional<{
@@ -10,24 +10,64 @@ interface ObjectConstructor {
   }>;
 }
 
+declare function parseInt<T extends string | number>(s: T): TSM.ParseIntegerWithTail<T>;
+declare function parseFloat<T extends string | number>(s: T): TSM.ParseNumberWithTail<T>;
+
+interface Window {
+  parseInt<T extends string | number>(s: T): TSM.ParseIntegerWithTail<T>;
+  parseFloat<T extends string | number>(s: T): TSM.ParseNumberWithTail<T>;
+}
+
+interface NumberConstructor {
+  parseInt<T extends string | number>(s: T): TSM.ParseIntegerWithTail<T>;
+  parseFloat<T extends string | number>(s: T): TSM.ParseNumberWithTail<T>;
+}
+
 /**
- * TypeScriptMagic
+ * **TypeScriptMagic**
  *
  * A namespace for more complicated utility types, so as not to pollute the global namespace.
  */
 declare namespace TSM {
+  /** Gets the keys of an object whose values are optional, `undefined`, or a union type including `undefined`. */
   type UndefinedKeys<T> = {
-    [K in keyof T]-?: undefined extends T[K] ? T[K] extends undefined ? never : K : never;
+    [K in keyof T]-?: undefined extends T[K] ? K : never;
   }[keyof T];
 
-  type MakeUndefinedValuesOptional<T> = Omit<T, UndefinedKeys<T>> & Partial<Pick<T, UndefinedKeys<T>>>;
+  /**
+   * Gets the keys of an object that have a union type including `undefined` values, e.g. `'x'` in `{ x?: 1 }` and `{ x:
+   * 1 | undefined }`. Intentionally does *not* capture keys whose values can only be `undefined`, e.g. `{ notCaptured:
+   * undefined }` and `{ notCaptured?: undefined }`.
+   */
+  type MaybeUndefinedKeys<T> = {
+    [K in keyof T]-?: undefined extends T[K] ? T[K] extends undefined ? never : K : never;
+  }[keyof T & (T extends any[] ? TSM.ArrayObjectKeys<T> : unknown)];
 
-  type IterableArrayKeys<T extends readonly any[]> =
+  /** Makes the values of an object that have `undefined` values optional. */
+  type MakeUndefinedValuesOptional<T> = Omit<T, MaybeUndefinedKeys<T>> & Partial<Pick<T, MaybeUndefinedKeys<T>>>;
+
+  /**
+   * @internal
+   *
+   * Gets only the keys an array or tuple that are not defined on the `Array<T>` prototype. Resolves to `never` if the
+   * array is empty (and there are no additionally defined keys--you can just set arbitrary string keys onto an array
+   * object). This is useful for `Object.keys()` and `Object.entries()` of any array.
+   *
+   * **Warning**
+   *
+   * *Unless you understand exactly how this type works, I recommend you do not use this directly.*
+   *
+   * For tuples, this will correctly resolve to numeric indices as strings. For `T extends readonly any[]`, this will
+   * return `number` rather than `` `${number}` ``. This is because TypeScript expects indices to be only numeric, and
+   * so the definition for `Object.entries()` needs to index the arrays as `number`, not as `` `${number}` ``. This is
+   * why this type is marked *@internal*.
+   */
+  type ArrayObjectKeys<T extends readonly any[]> =
     | Exclude<keyof Required<T>, symbol | keyof any[]>
     | (
       T extends readonly []
-        ? never
-        : T extends readonly [infer _, ...any] ? never : number
+      ? never
+      : T extends readonly [infer _, ...any] ? never : number
     );
 
   /** Applies `Partial<T>` to `T` and all descendants of `T`. */
@@ -39,4 +79,88 @@ declare namespace TSM {
   type DeepRequired<T> = {
     [K in keyof T]-?: DeepRequired<T[K]>;
   }
+
+  /** Applies `Readonly<T>` to `T` and all descendants of `T`. */
+  type DeepReadonly<T> = {
+    readonly [K in keyof T]: DeepReadonly<T[K]>;
+  }
+
+  /** Returns `true` if `T` is `any`, else `false`. */
+  type IsAny<T> = 0 extends (1 & T) ? true : false;
+
+  /** Returns `true` if `T` is `never`, else `false`. */
+  type IsNever<T> = [T] extends [never] ? true : false;
+
+  /**
+   * Returns the numeric value of a string that is exactly a number. (Also works with numbers, in which case it's the
+   * identity type.)
+   */
+  type ParseNumber<T extends string | number> =
+    T extends number ? T :
+    T extends `.${infer N extends number}` ? ParseNumber<`0.${N}`> :
+    T extends `${infer N extends number}.` ? N :
+    // handle case where the last digit is a 0
+    T extends `${infer N extends number}.${infer U}0` ? ParseNumber<`${N}.${U}`> :
+    T extends `${infer N extends number}` ? N :
+    never;
+
+  /**
+   * Returns the numeric value at the front of a string. (Also works with numbers, in which case it's the identity
+   * type.)
+   *
+   * **Warning**
+   *
+   * Will run into issues with strings > 50 characters long due to TypeScript's limitations on recursive types.
+   */
+  type ParseNumberWithTail<T extends string | number> =
+    T extends number ? T :
+    T extends `${number}` ? ParseNumber<T> :
+    T extends `${infer N extends number}${infer U}`
+      ? U extends `${number}${string}`
+        ? U extends `-${string}`
+          ? N
+          : U extends `${infer Head extends number}.${infer Tail extends number}${string}`
+            // handle parsing of things like `1.1.1`
+            ? `${Head}` extends `${string}.${string}`
+              ? TSM.ParseNumber<`${N}.${ParseIntegerWithTail<Head>}`>
+              : TSM.ParseNumber<`${N}${ParseInteger<Head>}.${ParseIntegerWithTail<Tail>}`>
+            : TSM.ParseNumber<`${N}${ParseNumberWithTail<U>}`>
+      : U extends `${number}${string}`
+        ? TSM.ParseNumber<`${N}${ParseNumberWithTail<U>}`>
+        : U extends `.${infer Decimal extends number}${string}`
+          ? TSM.ParseNumber<`${N}.${ParseIntegerWithTail<Decimal>}`>
+          : N :
+    never;
+
+  /**
+   * Returns the number value of a string or number that is an integer. Returns `never` if the input is a float or a
+   * string that is not an integer.
+   */
+  type ParseInteger<T extends string | number> =
+    T extends number ? ParseInteger<`${T}`> :
+    T extends `${infer I extends number}` ? T extends `${number}.${number}` ? never : I :
+    never;
+
+  /**
+   * Returns the integer value at the front of a string, or truncates float to be an integer (i.e. the return value of
+   * `parseInt(myFloatingPointNumber)`). Returns `never` if the input a string that does not begin with an integer.
+   *
+   * **Warning**
+   *
+   * Will run into issues with strings starting with numbers > 50 characters long due to TypeScript's limitations on
+   * recursive types. However, such a number would be far greater than `Number.MAX_SAFE_INTEGER` anyway.
+   */
+  type ParseIntegerWithTail<T extends string | number> =
+    T extends number ? ParseInteger<`${T}`> :
+    T extends `-.${infer U extends number}`
+      ? `${U}` extends `-${number}`
+        ? never
+        : 0 :
+    T extends `${infer I extends number}.${string}`
+      ? I
+      : T extends `${infer I extends number}${infer U}`
+        ? U extends `${number}${string}`
+          ? TSM.ParseInteger<`${I}${ParseIntegerWithTail<U>}`>
+        : I :
+    never;
 }
--- test.ts
+++ test.ts
@@ -33,6 +33,8 @@ const augArr = [1, 2, 3] as AugmentedArray<[1, 2, 3]>;
     // @ts-expect-error
     '3',
     // @ts-expect-error
+    0,
+    // @ts-expect-error
     'foo',
   ];
 }
@@ -73,7 +75,7 @@ const augArr = [1, 2, 3] as AugmentedArray<[1, 2, 3]>;
   const _: typeof r[number][] = [
     ['a', 0], ['b', false], ['c', { x: 2 }],
     // @ts-expect-error
-    ['c', { x: 'a'} ],
+    ['c', { x: 'a' }],
     // @ts-expect-error
     [TestSymbol, 2],
     // @ts-expect-error
@@ -111,12 +113,14 @@ const augArr = [1, 2, 3] as AugmentedArray<[1, 2, 3]>;
   const _: typeof r[number][] = [
     ['0', 1], ['1', 2], ['2', 3], ['foo', 'bar'], ['foo', undefined],
     // @ts-expect-error
+    ['3', 4],
+    // @ts-expect-error
     [TestSymbol, 2],
   ];
 }
 
 // Object.fromEntries() tests
-(): { a: 1; b: true; c: { x: 1; }; [TestSymbol]: 2 } => Object.fromEntries([['a', 1], ['b', true], ['c', { x: 1 }], [TestSymbol, 2]] as const);
+(): { a: 1; b: true; c: { x: 1; };[TestSymbol]: 2 } => Object.fromEntries([['a', 1], ['b', true], ['c', { x: 1 }], [TestSymbol, 2]] as const);
 (): { a: number; b: boolean; c: { x: number; }; } => Object.fromEntries(Object.entries({ a: 1, b: true, c: { x: 1 } }));
 (): { a: number; b?: boolean; } => Object.fromEntries(Object.entries<{ a: 1, b?: true }>({ a: 1 }));
 // @ts-expect-error
@@ -130,3 +134,142 @@ const augArr = [1, 2, 3] as AugmentedArray<[1, 2, 3]>;
   type Test = Foo extends typeof x ? true : false;
   const test: Test = true;
 }
+
+// parseFloat() tests
+(): 0.1 => parseFloat('.1');
+(): 0.1 => parseFloat('0.1');
+(): 1 => parseFloat('1');
+(): 1 => parseFloat('1.');
+(): 1 => parseFloat('1.0');
+(): 1 => parseFloat('1.00');
+(): 10 => parseFloat('10');
+(): 10 => parseFloat('10.0');
+(): 10.1 => parseFloat('10.1');
+(): 10 => parseFloat('10. and stuff');
+(): 1.1 => parseFloat('1.1');
+(): 1.1 => parseFloat('1.1 and stuff');
+(): -1.1 => parseFloat('-1.1');
+(): 1.1 => parseFloat(1.1);
+(): 10.1 => parseFloat('10.1.1');
+{
+  let x = parseFloat('.');
+  // @ts-expect-error
+  x = 1;
+}
+
+// parseInt() tests
+(): 0 => parseInt('0');
+(): 1 => parseInt('1');
+(): 1 => parseInt('1.0');
+(): 10 => parseInt('10');
+(): 10 => parseInt('10 and other things');
+(): 1 => parseInt('1.1');
+(): 1 => parseInt('1.1 and other things');
+(): 0 => parseInt('-.1');
+(): 1 => parseInt(1);
+(): 1 => parseInt(1.1);
+(): 10 => parseInt(10);
+(): -1 => parseInt('-1');
+(): -1 => parseInt('-1.1');
+// @ts-expect-error
+(): 2 => parseInt(1);
+// @ts-expect-error
+(): 2 => parseInt('1');
+// @ts-expect-error
+(): 2 => parseInt('1.2');
+
+// TSM.ParseNumber tests
+(): TSM.ParseNumber<1.1> => 1.1;
+(): TSM.ParseNumber<'1.1'> => 1.1;
+(): TSM.ParseNumber<-.1> => -.1;
+(): TSM.ParseNumber<'-.1'> => -.1;
+// @ts-expect-error
+(): TSM.ParseNumber<1.1> => 1;
+// @ts-expect-error
+(): TSM.ParseNumber<1.1> => 1.2;
+// @ts-expect-error
+(): TSM.ParseNumber<'1.1'> => 1;
+// @ts-expect-error
+(): TSM.ParseNumber<'1.1'> => 2;
+
+// TSM.ParseNumber tests
+(): TSM.ParseNumber<1.1> => 1.1;
+(): TSM.ParseNumber<'1.1'> => 1.1;
+
+// TSM.ParseInteger tests
+(): TSM.ParseInteger<'1'> => 1;
+
+// TSM.IsAny tests
+(): true => true as TSM.IsAny<any>;
+(): false => false as TSM.IsAny<unknown>;
+(): false => false as TSM.IsAny<never>;
+(): false => false as TSM.IsAny<number>;
+
+// TSM.IsNever tests
+(): true => true as TSM.IsNever<never>;
+(): false => false as TSM.IsNever<any>;
+(): false => false as TSM.IsNever<unknown>;
+(): false => false as TSM.IsNever<number>;
+
+// TSM.DeepReadonly tests
+{
+  const x: TSM.DeepReadonly<{ a: 1, b: { c: 2 }, d: Record<string, number> }> = { a: 1, b: { c: 2 }, d: {} };
+  // @ts-expect-error
+  x.a = 2;
+  // @ts-expect-error
+  x.b.c = 3;
+  // @ts-expect-error
+  x.d.foo = 4;
+}
+
+// TSM.DeepRequired tests
+(): TSM.DeepRequired<{ a?: 1, b?: { c?: 2 } }> => ({ a: 1, b: { c: 2 } });
+// @ts-expect-error
+(): TSM.DeepRequired<{ a?: 1, b?: { c?: 2 } }> => ({ b: { c: 2 } });
+// @ts-expect-error
+(): TSM.DeepRequired<{ a?: 1, b?: { c?: 2 } }> => ({ a: 1, b: {} });
+
+// TSM.DeepPartial tests
+(): TSM.DeepPartial<{ a: 1, b: { c: 2 } }> => ({});
+(): TSM.DeepPartial<{ a: 1, b: { c: 2 } }> => ({ a: 1 });
+(): TSM.DeepPartial<{ a: 1, b: { c: 2 } }> => ({ b: {} });
+(): TSM.DeepPartial<{ a: 1, b: { c: 2 } }> => ({ b: { c: 2 } });
+(): TSM.DeepPartial<{ a: 1, b: { c: 2 } }> => ({ a: undefined, b: { c: 2 } });
+// @ts-expect-error
+(): TSM.DeepPartial<{ a: 1, b: { c: 2 } }> => ({ d: 1 });
+// @ts-expect-error
+(): TSM.DeepPartial<{ a: 1, b: { c: 2 } }> => ({ a: 2 });
+// @ts-expect-error
+(): TSM.DeepPartial<{ a: 1, b: { c: 2 } }> => ({ b: [] });
+// @ts-expect-error
+(): TSM.DeepPartial<{ a: 1, b: { c: 2 } }> => ({ b: { d: 2 } });
+
+// TSM.UndefinedKeys tests
+(): TSM.UndefinedKeys<{ a: 1, b?: 2, c: 3 | undefined, d: undefined, e?: undefined }> => 'b';
+(): TSM.UndefinedKeys<{ a: 1, b?: 2, c: 3 | undefined, d: undefined, e?: undefined }> => 'c';
+(): TSM.UndefinedKeys<{ a: 1, b?: 2, c: 3 | undefined, d: undefined, e?: undefined }> => 'd';
+(): TSM.UndefinedKeys<{ a: 1, b?: 2, c: 3 | undefined, d: undefined, e?: undefined }> => 'e';
+// @ts-expect-error
+(): TSM.UndefinedKeys<{ a: 1, b?: 2, c: 3 | undefined, d: undefined, e?: undefined }> => 'a';
+// @ts-expect-error
+(): TSM.UndefinedKeys<{ a: 1, b?: 2, c: 3 | undefined, d: undefined, e?: undefined }> => 'f';
+
+// TSM.MaybeUndefinedKeys tests
+(): TSM.MaybeUndefinedKeys<{ a: 1, b?: 2, c: 3 | undefined, d: undefined, e?: undefined }> => 'b';
+(): TSM.MaybeUndefinedKeys<{ a: 1, b?: 2, c: 3 | undefined, d: undefined, e?: undefined }> => 'c';
+(): TSM.MaybeUndefinedKeys<[ undefined, 1 | undefined, 2? ]> => '1';
+(): TSM.MaybeUndefinedKeys<[ undefined, 1 | undefined, 2? ]> => '2';
+// @ts-expect-error
+(): TSM.MaybeUndefinedKeys<[ undefined, 1 | undefined, 2? ]> => 3;
+// @ts-expect-error
+(): TSM.MaybeUndefinedKeys<{ a: 1, b?: 2, c: 3 | undefined, d: undefined, e?: undefined }> => 'a';
+// @ts-expect-error
+(): TSM.MaybeUndefinedKeys<{ a: 1, b?: 2, c: 3 | undefined, d: undefined, e?: undefined }> => 'd';
+// @ts-expect-error
+(): TSM.MaybeUndefinedKeys<{ a: 1, b?: 2, c: 3 | undefined, d: undefined, e?: undefined }> => 'e';
+// @ts-expect-error
+(): TSM.MaybeUndefinedKeys<[ undefined, 1 | undefined, 2? ]> => '0';
+// @ts-expect-error
+(): TSM.MaybeUndefinedKeys<[ undefined, 1 | undefined, 2? ]> => 0;
+// @ts-expect-error
+(): TSM.MaybeUndefinedKeys<[ undefined, 1 | undefined, 2? ]> => 1;
